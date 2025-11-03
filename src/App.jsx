941235bb853a1bb352bfdc72348e9eecd54b446e
Vahe Sevachyan <vscode13@gmail.com>
2025-11-03 15:18:44 -0800

    added email,phone functionality to personal info section

--- src/App.jsx
+++ src/App.jsx
@@ -6,20 +6,33 @@ import { PersonalInfo } from "./Components/PersonalInfo";
 function App() {
   const [firstName, setFirstName] = useState("");
   const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("");
+
   const handleFirstNameChange = (event) => {
     setFirstName(event.target.value);
   };
   const handleLastNameChange = (event) => {
     setLastName(event.target.value);
   };
+  function handleEmailChange(event) {
+    setEmail(event.target.value);
+  }
+  function HandlePhoneNumberChange(event) {
+    setPhoneNumber(event.target.value);
+  }
   return (
     <>
-      <h2>First Name: {firstName}</h2>
-      <h2>Last Name: {lastName}</h2>
       <PersonalInfo
         FirstNameChange={handleFirstNameChange}
         LastNameChange={handleLastNameChange}
+        EmailChange={handleEmailChange}
+        PhoneNumberChange={HandlePhoneNumberChange}
       />
+      <h2>First Name: {firstName}</h2>
+      <h2>Last Name: {lastName}</h2>
+      <h2>Email: {email}</h2>
+      <h2>Phone Number:{phoneNumber}</h2>
     </>
   );
 }
--- src/Components/PersonalInfo.jsx
+++ src/Components/PersonalInfo.jsx
@@ -1,8 +1,15 @@
-export const PersonalInfo = ({ FirstNameChange, LastNameChange }) => {
+export const PersonalInfo = ({
+  FirstNameChange,
+  LastNameChange,
+  EmailChange,
+  PhoneNumberChange,
+}) => {
   return (
     <div>
       <input type="text" placeholder="First Name" onChange={FirstNameChange} />
       <input type="text" placeholder="Last Name" onChange={LastNameChange} />
+      <input type="email" placeholder="Email" onChange={EmailChange} />
+      <input type="number" placeholder="Phone" onChange={PhoneNumberChange} />
     </div>
   );
 };
